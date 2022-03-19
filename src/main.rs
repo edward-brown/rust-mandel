95f1f9f4a17966c1acb33daf80495218440dd6ee
Edward Brown <eab.brown2@gmail.com>
2022-03-19 19:43:01 +0000

    Added mapping (palette stolen from google)

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let mut pixel_data = vec![0; bounds.0 * bounds.1 * 3];
     render(bounds.0, bounds.1, top_left, bottom_right, &mut pixel_data);
     write_image("output.png", &pixel_data, bounds)
-    .expect("Error writing file");
+        .expect("Error writing file");
 
     println!("Elapsed: {:.2?}", now.elapsed());
 }
@@ -29,6 +29,25 @@ fn write_image(filename: &str, pixel_data: &[u8], bounds: (usize, usize)) -> Res
 }
 
 fn render(width: usize, height: usize, top_left: Complex<f64>, bottom_right: Complex<f64>, pixel_data: &mut [u8]) {
+    let mapping: [[u8; 3]; 16] = [
+        [66, 30, 15],
+        [25, 7, 26],
+        [9, 1, 47],
+        [4, 4, 73],
+        [0, 7, 100],
+        [12, 44, 138],
+        [24, 82, 177],
+        [57, 125, 209],
+        [134, 181, 229],
+        [211, 236, 248],
+        [241, 233, 191],
+        [248, 201, 95],
+        [255, 170, 0],
+        [204, 128, 0],
+        [153, 87, 0],
+        [0, 0, 0]
+    ];
+
     let mut count = 0;
     for h in 0..height {
         for w in 0..width {
@@ -36,10 +55,16 @@ fn render(width: usize, height: usize, top_left: Complex<f64>, bottom_right: Com
             let c = pixel_to_mandelspace((w, h), (width, height), top_left, bottom_right);
             let ic = iteration_count(c);
 
-            // Calc colors
-            let r = (255.0 * c.norm_sqr()) * ic as f64;
-            let g = (255.0) * ic as f64;
-            let b = (20.0 * c.norm_sqr()) * ic as f64;
+            let mut r = 0;
+            let mut g = 0;
+            let mut b = 0;
+
+            if ic != 0 {
+                let map = ic % 16;
+                r = mapping[map as usize][0];
+                g = mapping[map as usize][1];
+                b = mapping[map as usize][2];
+            }
 
             // Set pixel data
             pixel_data[count + 0] = r as u8;
